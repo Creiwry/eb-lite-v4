e75200359625e0d31bffd3df3827a3179276b2b1
Creiwry <jay.gardos@outlook.com>
2022-12-20 15:05:40 +0100

    feat(controllers): add resources and controllers for participations and events

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :events
+  resources :participations
   # Defines the root path route ("/")
   get 'static_pages/secret'
 
