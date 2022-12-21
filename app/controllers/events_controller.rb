7c637abcc853d58c1e39c331a5d5a974875c413f
Creiwry <jay.gardos@outlook.com>
2022-12-21 19:36:27 +0100

    feat(views, controller): add edit event page with destroy functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:destroy]
   def index
     @events = Event.all
 
@@ -31,18 +32,42 @@ class EventsController < ApplicationController
       redirect_to event_path(@event.id)
     else
 
-      redirect_to user_path(current_user.id)
+      render :new
     end
 
     
   end
 
   def edit
+    @user = current_user
+    @event = Event.find(params[:id])
   end
 
   def update
+    @user = current_user
+    @event = Event.find(params[:id])
+    event_params = params[:user]
+    @event.update(
+      title: event_params[:title],
+      description: event_params[:description],
+      start_date: event_params[:start_date],
+      duration: event_params[:duration],
+      price: event_params[:price],
+      location: event_params[:location]
+    )
+
+    if @event.save
+      redirect_to event_path(@event.id)
+    else
+
+      render :edit
+    end
   end
 
   def destroy
+    @user = current_user
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to user_path(@user.id)
   end
 end
