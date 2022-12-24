8abc53b7b49c52dcf2584e9324146927b20a5648
Creiwry <jay.gardos@outlook.com>
2022-12-24 13:39:36 +0100

    feat(services): add service to create a stripe product

---
+++ app/services/stripe_product_creator.rb
@@ -0,0 +1,11 @@
+class StripeProductCreator
+  def initialize(event_id)
+    @event = Event.find(event_id)
+  end
+
+  def create
+    Stripe.api_key = Rails.application.credentials[:stripe][:private_key]
+    @product = Stripe::Product.create({name: @event.title })
+    @event.update(product: @product.id)
+  end
+end
