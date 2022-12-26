c92caf308d765a55359bdac0d4d7c7ae2def65b5
Creiwry <jay.gardos@outlook.com>
2022-12-26 21:11:18 +0100

    chore(controllers): move authenticate_user method into Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   require 'stripe'
-  
+
+  def authenticate_user
+    return if current_user
+
+    flash[:danger] = 'Please log in.'
+    redirect_to new_user_session_path
+  end
 end
