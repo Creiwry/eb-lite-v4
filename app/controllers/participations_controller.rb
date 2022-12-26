5708d6786fdc93867bee0d290c6b7bd070131b93
Creiwry <jay.gardos@outlook.com>
2022-12-26 21:18:44 +0100

    feat(controllers): add create participation functionality

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,5 +1,8 @@
-class ParticipationsController < ApplicationController
+# frozen_string_literal: true
 
+# Participations Controller
+class ParticipationsController < ApplicationController
+  before_action :authenticate_user, only: %i[new create edit update delete]
 
   def index
   end
@@ -8,32 +11,14 @@ class ParticipationsController < ApplicationController
   end
 
   def create
-    @user = current_user
-    @event = Event.find(params[:event_id])
-    @stripe_customer = Stripe::Customer.create({
-      email: @user.email,
-    })
-
-    @participation = Participation.new(
-      event_id: @event.id,
-      participant_id: @user.id,
-      stripe_customer_id: @stripe_customer.id
-    )
-
-
-    if @participation.save
-      flash[:notice] = 'Ticket successfully purchased'
-      redirect_to user_path(@user.id)
-    else
-      render :new
-    end
-
-  end
-
-  def new
-
     @event = Event.find(params[:event_id])
     Stripe.api_key = Rails.application.credentials[:stripe][:private_key]
+
+    success_url = url_for(controller: 'successes',
+                          action: 'show',
+                          event_id: @event.id,
+                          host: ENV["HOST_URL"])
+
 
     @event_price_id = Stripe::Price.create({
       unit_amount: @event.price * 100,
@@ -43,16 +28,41 @@ class ParticipationsController < ApplicationController
     current_user.set_payment_processor :stripe
     @checkout_session = current_user.payment_processor.checkout(
       mode: 'payment',
-      line_items: @event_price_id
-    )
+      line_items: @event_price_id,
+      success_url: success_url
+      )
+
+    begin
     rescue Stripe::CardError => e
       flash[:alert] = e.message
       redirect_to new_participation_path
     end
 
-    if @checkout_session.status == 'succeeded'
-      redirect_to :action => 'create', :event_id =>@event.id
+    @user = current_user
+    @stripe_customer = Stripe::Customer.create({
+      email: @user.email
+    })
+
+    @participation = Participation.new(
+      event_id: @event.id,
+      participant_id: @user.id,
+      stripe_customer_id: @stripe_customer.id
+    )
+    
+    redirect_to @checkout_session.url, allow_other_host: true
+
+    puts @checkout_session.status
+
+    if @participation.save
+      flash[:notice] = 'Ticket successfully purchased'
+    else
+      render :new
     end
+
+  end
+
+  def new
+    @event = Event.find(params[:event_id])
   end
 
   def edit
